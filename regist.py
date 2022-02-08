442a3ecd9aa961b5971dfb9e727e7e25f0849493
reXt88 <seva88best.com@yandex.ru>
2022-02-08 14:32:08 +0000

    Regiistration

--- regist.py
+++ regist.py
@@ -18,4 +18,21 @@ if log not in q:
 	os.system("./autor.py")
 else:
 	print("Username already exists")
+	print("")
+	print("Do you want registration again or log in?")
+	print("              1                      2")
+	b = input()
+	if b=="1":
+		log = input("Entr login for registration ")
+		passw = input("Enter password for registration ")
+		print("Registration complete")
+		logins = open("filelogins.txt", "a")
+		logins.write(f"{log}\n")
+		logins.close()
+		passwords = open("filepasswords.txt", "a")
+		passwords.write(f"{passw}\n")
+		passwords.close()
+		os.system("./autor.py")
+	else:
+		os.system("./autor.py")
 
