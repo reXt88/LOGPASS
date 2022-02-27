35c0a5a5c55c55ea139a6a28e5fdeda620594d17
reXt88 <seva88best.com@yandex.ru>
2022-02-27 13:18:38 +0000

    Registration

--- regist.py
+++ regist.py
@@ -1,35 +1,33 @@
 #!/bin/python3
+
 import os
 import os.path
 
 print("-------------------------------------------")
 c=0
 while c!=1:
-	log = input("Enter login for registration ")
-#Открытие файла для считывания уже существующих логинов
-	a = open("filepasswords.txt" , "r")
-	q = a.read()
-	a.close()
-#Если логина в файле нет - продолжается регистрация
-	if log not in q:
-		passw = input("Enter password for registration ")
-		print("Registration complete")
-		print("-------------------------------------------\n\n\n\n\n")
-		passwords = open("filepasswords.txt", "a")
-		passwords.write(f"{log}:{passw}\n")
-		passwords.close()
-		os.system("./autor.py")
-		break
-#В противном случае прдоставляется выбор на вход в существующий аккаунт или повторная регистрация
-	else:
-		print("Username already exists")
-		b=""
-		while b!="yes" or b!="no":
-			b=input("Do you want registration again?(yes/no)")
-			if b=="yes" or b=="y":
-				break
-			if b=="no" or b=="n":
-				c=1
-				print("-------------------------------------------")
-				break
-		
+    log = input("Enter login for registration ")
+    #Открытие файла для проверки уже существующих логинов
+    a = open("filepasswords.txt", "r")
+    a.close()
+    #Если лоигна нет в файле - продолжается регистраия
+    if log not in q:
+        passw = input("Enter password for registration ")
+        print("Registration complete")
+        print("-------------------------------------------")
+        passwords = open("filepasswords", "a")
+        passwords.write(f"{log}:{passw}\n")
+        passwords.close()
+        os.system("./autor.py")
+        break
+    else:
+        print ("Username already exists")
+        b=""
+        while b!="yes" or b!="no":
+            b = input("Do you want to registration again?(yes/no)")
+            if b=="yes" or b=="y":
+                break
+            if b=="no" or b=="n":
+                c=1
+                print("-------------------------------------------")
+                break
